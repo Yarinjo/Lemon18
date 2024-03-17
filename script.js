16165997c8c50f468ae605a09366b18cfb88bfb3
Yarinjo <gladysyora@gmail.com>
2024-03-17 13:17:56 +0200

    added upgraded version of homework 18

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function addColorAnimation () {
 	serviceBlock.classList.toggle('color-change-2x-background')
 };
 serviceBlock.addEventListener('mouseover', addColorAnimation)
+
+
+
+
+
+function navToggle() {
+	document.getElementById('header__navID').classList.toggle('is-active');
+  }
+  function navRemove() {
+	document.getElementById('header__navID').classList.remove('is-active');
+  }
+  function navAdd() {
+	document.getElementById('header__navID').classList.add('is-active');
+  }
+  
+  
+  const myBurger = document.getElementById('header__navID');
+  console.log(myBurger)
+  
+  const menu = document.querySelector('.menu')
+  document.addEventListener('click', (e) => {
+	const click = e.composedPath().includes(myBurger);
+	const menus = e.composedPath().includes(menu);
+	console.log(!click, !menus, myBurger.classList == 'is-active');
+	if (!click && !menus) {
+	  navRemove();
+	}
+	}
+  );
